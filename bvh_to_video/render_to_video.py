3b1f2cc2d23e71cdd149874140480261d0a010c2
Vikram Kumar <32137511+Vikram1511@users.noreply.github.com>
2019-06-03 15:39:34 +0530

    Update render_to_video.py
    updated code for background image

--- bvh_to_video/render_to_video.py
+++ bvh_to_video/render_to_video.py
@@ -2,14 +2,11 @@ import bpy
 import os
 from math import *
 from mathutils import *
+import random
     
 bpy.ops.object.select_all(action='TOGGLE')
 bpy.ops.object.select_all(action='TOGGLE')
 bpy.ops.object.delete(use_global=False)
-
-mhx2path = "C:/Users/Vikram Jain/Documents/GitHub/yoga-pose-estimation/characters"
-bvhpath = "C:/Users/Vikram Jain/Documents/GitHub/yoga-pose-estimation/bvhFiles"
-OutputDirPath = "C:/Users/Vikram Jain/Documents/GitHub/yoga-pose-estimation/Animation"
 
 def getFiles(folderpath,extension):
     path = folderpath
@@ -21,56 +18,47 @@ def getFiles(folderpath,extension):
                 files.append(os.path.join(r, file))
     return files
 
-#to get mhx2 files and bvh files in list
+# filep1 = "~/Downloads/person2.mhx2"
+# filep2 = "/Users/daddysHome/Downloads/tpose_sample.mhx2"
 
-
-MHX2List = getFiles(mhx2path,".mhx2")
-BVHList = getFiles(bvhpath,".bvh")
-#defining number of scenes which is equal to number of characters we have
+# filebvh1 = "/Users/daddysHome/Downloads/kinect2bvh 2/Subj002_Katichakrasana_joints_processed.bvh"
+MHX2List = getFiles("/Users/daddysHome/Downloads",".mhx2")
+BVHList = getFiles("/Users/daddysHome/Downloads/kinect2bvh 2",".bvh")
+backgroundImages = getFiles("/Users/daddysHome/Downloads/backgroundImage",".jpg")
+for a in bpy.context.screen.areas:
+        if a.type == "VIEW_3D":
+            break
+a.spaces[0].show_background_images=True
+img = bpy.data.images.load(backgroundImages[0])
 scn = len(MHX2List)
-
-#loop for each scene 
+texture = bpy.data.textures.new("Texture.001","IMAGE")
+bpy.data.worlds['World'].active_texture = texture
+bpy.context.scene.world.texture_slots[0].use_map_horizon=True
+bpy.data.worlds[0].use_sky_paper = True
 for i in range(scn):
-	#adding camera
-    bpy.ops.object.camera_add()   
-
-    #adding lamp                         																	 
-    bpy.ops.object.lamp_add(type='HEMI')	
-
-    #importing make human file in blender from list which contains all the files with extension ".mhx2" with their path																	
-    bpy.ops.import_scene.makehuman_mhx2(filepath = MHX2List[i])		
-
-    #setting current scene's camera for rendering the current scene's animation
+    bpy.context.scene.world = bpy.data.worlds[0]
+    texture.image=img
+    # scene = bpy.context.screen.scenes
+    # screen = bpy.context.screen
+    bpy.ops.object.camera_add()
+    bpy.ops.object.lamp_add(type='HEMI')
+    bpy.ops.import_scene.makehuman_mhx2(filepath = MHX2List[i])
+    person = bpy.data.scenes[i].objects[2].name
     bpy.context.scene.camera = bpy.data.objects[bpy.data.cameras[i].name]
-
-    #setting location for camera,lamp for current scene which will be same for all the scene
     bpy.data.scenes[i].objects[bpy.data.cameras[i].name].location = Vector((-1.6719582080841064, -41.433406829833984, 4.720533847808838))
     bpy.data.scenes[i].objects[bpy.data.cameras[i].name].rotation_euler = Euler((1.6681835651397705, -0.0017817476764321327, 6.209451198577881), 'XYZ')
     bpy.data.scenes[i].objects[bpy.data.cameras[i].name].scale = Vector((1.4308398962020874, 1.4057042598724365, 1.8842936754226685))
     bpy.data.scenes[i].objects[bpy.data.lamps[i].name].location = Vector((-1.6768434047698975, -5.329115867614746, 38.93578338623047))
     bpy.data.scenes[i].objects[bpy.data.lamps[i].name].rotation_euler = Euler((0.032153837382793427, 0.0016589768929407, 4.444016933441162), 'XYZ')
-
-    #load_and_retarget bvh files
-    #looping through all the bvh files which exist in our list "BVHList" and retarget for each character in one scene
     for j in range(len(BVHList)):
         bpy.ops.mcp.load_and_retarget(filter_glob = ".bvh",filepath = BVHList[j])
-
-        #setting frames start and end , we can change accordingly or even can find the appropriate frames for each bvh file and set it as end frame value
         bpy.data.scenes[i].frame_start = 1
         bpy.data.scenes[i].frame_end = 50
-
-        #setting image file format for rendering
         bpy.data.scenes[i].render.image_settings.file_format = 'FFMPEG'
-
-        #setting directory for rendered animation video which will have multiple directory and named by character name and will have all the animation videos for that character
-        bpy.data.scenes[i].render.filepath = OutputDirPath+MHX2List[i][len(mhx2path):len(MHX2List[i])-5]+"/"+BVHList[j][len(bvhpath):len(BVHList[j])-4]
-        
-        #to not overwrite the animated videos
+        bpy.data.scenes[i].render.filepath = "/Users/daddysHome/Downloads/animation/"+MHX2List[i][28:len(MHX2List[i])-5]+"/"+BVHList[j][40:len(BVHList[j])-5]
         bpy.context.scene.render.use_overwrite = False
-        bpy.ops.render.render(animation = True)
+        bpy.ops.render.render(animation=True)
+        
 
-    #after rendering all videos for one character, we will generate new scene in blender for next character
     bpy.ops.scene.new(type="NEW")
-    
-    #set new generated scene as context scene
-    bpy.context.screen.scene=bpy.data.scenes[i+1]
+    # bpy.context.scene=bpy.data.scenes[i+1]
