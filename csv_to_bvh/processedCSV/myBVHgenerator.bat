fb45e80660b9e2e12095c193b890571c88e1baa9
Nihar Patel <nihar5733@gmail.com>
2019-06-06 11:32:03 +0530

    done

--- csv_to_bvh/processedCSV/myBVHgenerator.bat
+++ csv_to_bvh/processedCSV/myBVHgenerator.bat
@@ -1,3 +1,3 @@
 cd ../main
-bash generateBVH.sh "../processedCSV/pp.txt" "../BVHgenerated"
+bash generateBVH.sh "../processedCSV/pp.txt" "../../bvhFiles"
 cd ../processedCSV
---
+++ generate.bat
@@ -0,0 +1,5 @@
+cd csv_to_bvh
+./toBVH.bat
+
+cd C:\Program Files\Blender Foundation\Blender
+blender -b -P "D:/IITD/Summer_2019/yoga-pose-estimation/bvh_to_video/render_to_video.py"
