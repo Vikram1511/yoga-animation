b89ed0028ddcefa16e1ea9b79e07d2acc6fba819
Vikram1511 <vikramj021@gmail.com>
2019-06-12 16:08:47 +0530

    changes

--- bvh_to_video/render_to_video.py
+++ bvh_to_video/render_to_video.py
@@ -41,6 +41,8 @@ bpy.data.worlds['World'].active_texture = texture
 bpy.context.scene.world.texture_slots[0].use_map_horizon=True
 bpy.data.worlds[0].use_sky_paper = True
 rootlen = len(mhx2path)
+
+
 for i in range(scn):
     bpy.context.scene.world = bpy.data.worlds[0]
     texture.image=img
@@ -51,13 +53,11 @@ for i in range(scn):
     bpy.ops.import_scene.makehuman_mhx2(filepath = MHX2List[i],useOverride = True,rigType = "MHX")
     # for fk to ik switching of leg bones
 
-    bpy.ops.mhx2.toggle_fk_ik(toggle="MhaLegIk_L 1 4 5")
-    bpy.ops.mhx2.toggle_fk_ik(toggle="MhaLegIk_R 1 20 21")
+    #bpy.ops.mhx2.toggle_fk_ik(toggle="MhaLegIk_L 1 4 5")
+    #bpy.ops.mhx2.toggle_fk_ik(toggle="MhaLegIk_R 1 20 21")
     ## bpy.ops.mhx2.toggle_fk_ik(toggle="MhaArmIk_L 1 2 3")   - arm is not necessary
     ## bpy.ops.mhx2.toggle_fk_ik(toggle="MhaArmIk_R 1 18 19")  - arm is not necessary
     person_name = MHX2List[i][rootlen+1:-5].capitalize()
-    # for bone in bpy.data.objects[person_name].pose.bones:
-    #     bone.rotation_mode = "ZXY"
     person = bpy.data.scenes[i].objects[2].name
     bpy.context.scene.camera = bpy.data.objects[bpy.data.cameras[i].name]
     bpy.data.scenes[i].objects[bpy.data.cameras[i].name].location = Vector((0, -5, 0))
@@ -65,6 +65,25 @@ for i in range(scn):
     bpy.data.scenes[i].objects[bpy.data.cameras[i].name].scale = Vector((0.5, 0.5, 0.5))
     bpy.data.scenes[i].objects[bpy.data.lamps[i].name].location = Vector((-1.6768434047698975, -5.329115867614746, 38.93578338623047))
     bpy.data.scenes[i].objects[bpy.data.lamps[i].name].rotation_euler = Euler((0.032153837382793427, 0.0016589768929407, 4.444016933441162), 'XYZ')
+    
+    bpy.data.scenes[i].render.engine="CYCLES"
+    bpy.data.scenes[i].cycles.device="GPU"
+
+    prefs = bpy.context.user_preferences
+    cprefs = prefs.addons['cycles'].preferences
+
+    # Attempt to set GPU device types if available
+    for compute_device_type in ('CUDA', 'OPENCL', 'NONE'):
+        try:
+            cprefs.compute_device_type = compute_device_type
+            break
+        except TypeError:
+            pass
+
+    # Enable all CPU and GPU devices
+    for device in cprefs.devices:
+            device.use = True
+            
     for j in range(len(BVHList)):
         file_bvh = BVHList[j]
         arr = file_bvh.split(".")
@@ -75,20 +94,23 @@ for i in range(scn):
         frame_range = bpy.data.objects[person_name].animation_data.action.frame_range[1]
         #for simplyfying f curves 
 
-        bpy.data.scenes[i].McpShowIK=True
-        bpy.data.scenes[i].McpFkIkArms=False
-        bpy.ops.mcp.transfer_to_ik()
+        #bpy.data.scenes[i].McpShowIK=True
+        #bpy.data.scenes[i].McpFkIkArms=False
+        #bpy.ops.mcp.transfer_to_ik()
         bpy.ops.mcp.simplify_fcurves()
-        bpy.ops.graph.simplify(error=0.95)
+        bpy.ops.graph.simplify(error=0.10)
         bpy.data.scenes[i].frame_start = 1
-        bpy.data.scenes[i].frame_end = 20
+        bpy.data.scenes[i].frame_end = frame_end
         bpy.data.scenes[i].frame_step = 1
         bpy.data.scenes[i].render.fps=30
         bpy.data.scenes[i].render.image_settings.file_format = 'FFMPEG'
         bpy.data.scenes[i].render.filepath = OutputDirPath+MHX2List[i][len(mhx2path):len(MHX2List[i])-5]+"/"+BVHList[j][len(bvhpath):len(BVHList[j])-4]
         bpy.context.scene.render.use_overwrite = False
+        
+        
+    
         bpy.ops.render.render(animation=True)
         
 
-    bpy.ops.scene.new(type="NEW")
-    # bpy.context.scene=bpy.data.scenes[i+1]
+        bpy.ops.scene.new(type="NEW")
+        # bpy.context.scene=bpy.data.scenes[i+1]
