51103419deec0df4aa96912d331dea314d36e877
Vikram Kumar <32137511+Vikram1511@users.noreply.github.com>
2019-07-06 15:17:58 +0530

    Delete generate.bat

--- generate.bat
+++
@@ -1,5 +0,0 @@
-cd csv_to_bvh
-./toBVH.bat
-
-cd C:\Program Files\Blender Foundation\Blender
-blender -b -P "C:/Users/Vikram Jain/Documents/GitHub/yoga-pose-estimation/bvh_to_video/render_to_video.py"
